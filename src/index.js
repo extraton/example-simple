3c57a126163be99f2268fdfdda96a2782cd3515b
qwertys318 <343604@gmail.com>
2021-03-10 13:52:50 +0700

    save

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// import freeton from "../freeton_dev/src/index";
-import freeton from "freeton";
+import freeton from "../freeton_dev/src/index";
+// import freeton from "freeton";
 
 const Kington = require('../contracts/Kington.json');
 const KingtonOrder = require('../contracts/KingtonOrder.json');
-// const SetcodeMultisigWallet = require('../contracts/SetcodeMultisigWallet.json');
+const SetcodeMultisigWallet = require('../contracts/SetcodeMultisigWallet.json');
 
 const _ = {
   checkExtensionAvailability() {
@@ -24,7 +24,11 @@ window.app = {
       _.checkExtensionAvailability();
       const provider = _.getProvider();
       const version = await provider.getVersion();
+      document.getElementById('result').innerHTML += '</br>' + JSON.stringify(version)
       console.log(version);
+    } catch (e) {
+      document.getElementById('result').innerHTML += '</br>' + JSON.stringify(e);
+      console.log(e);
     } finally {
       button.disabled = false;
     }
@@ -83,34 +87,50 @@ window.app = {
       const signer = await provider.getSigner();
       const wallet = signer.getWallet();
       const address = '0:b1c6ca063c050c4923580d3461c46c978651c4f100955e8ba32d40b278907206';
-      const amount = '1000000000';
+      const amount = '100000000';
       const payload = 'te6ccgEBAgEADgABCAAAAAABAApIZWxsbw==';
-      const contractMessageProcessing = await wallet.transfer(address, amount, true, payload);
+      const contractMessageProcessing = await wallet.transfer(address, amount, false, payload);
       await contractMessageProcessing.wait();
       console.log(`Transferred. TxId: ${contractMessageProcessing.txid}`)
     } finally {
       button.disabled = false;
     }
+  },
+  async confirmTransaction(form) {
+    const button = document.getElementById('buttonConfirmTransaction');
+    button.disabled = true;
+    try {
+      _.checkExtensionAvailability();
+      const provider = _.getProvider();
+      const signer = await provider.getSigner();
+      console.log({publicKey: signer.getPublicKey()});
+      const wallet = new freeton.Wallet(signer, form.address.value);
+      const contractMessageProcessing = await wallet.confirmTransaction(form.txid.value);
+      await contractMessageProcessing.wait();
+      console.log(`Confirmed. TxId: ${contractMessageProcessing.txid}`)
+    } finally {
+      button.disabled = false;
+    }
+  },
+  async run() {
+    const button = document.getElementById('buttonRun');
+    button.disabled = true;
+    try {
+      _.checkExtensionAvailability();
+      const provider = _.getProvider();
+      const signer = await provider.getSigner();
+      const contract = new freeton.Contract(signer, SetcodeMultisigWallet.abi, '0:138a41d0fafae5d6e9f7a19241702074e1f76858ebbe7bbc693bb8de813ca982');
+      const input = {
+        dest: '0:11684118bc3062a07126191bf17a650dbb101aff809eb79a9c64b061f4b9b97b',
+        value: '500000000',
+        bounce: false,
+        allBalance: false,
+        payload: '',
+      };
+      const result = await contract.functions.submitTransaction.run(input);
+      console.log(result);
+    } finally {
+      button.disabled = false;
+    }
   }
-  // async run() {
-  //   const button = document.getElementById('buttonRun');
-  //   button.disabled = true;
-  //   try {
-  //     _.checkExtensionAvailability();
-  //     const provider = _.getProvider();
-  //     const signer = await provider.getSigner();
-  //     const contract = new freeton.Contract(signer, SetcodeMultisigWallet.abi, '0:1ec56ec6f6e510f2e3843663d3e400ceda743a1ad2dcadbd347b0234338795db');
-  //     const input = {
-  //       dest: '0:11684118bc3062a07126191bf17a650dbb101aff809eb79a9c64b061f4b9b97b',
-  //       value: '500000000',
-  //       bounce: false,
-  //       allBalance: false,
-  //       payload: '',
-  //     };
-  //     const result = await contract.functions.submitTransaction.run(input);
-  //     console.log(result);
-  //   } finally {
-  //     button.disabled = false;
-  //   }
-  // }
 };

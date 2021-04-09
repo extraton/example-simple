7a8de7a8997278eb732a7c2761dd65305c1bb204
qwertys318 <343604@gmail.com>
2021-04-09 13:41:15 +0700

    wallet.tokens; contract.functions => contract.methods

--- src/index.js
+++ src/index.js
@@ -45,14 +45,14 @@ window.app = {
       button.disabled = false;
     }
   },
-  async getMessages() {
-    const button = document.getElementById('buttonGetMessages');
+  async runContractMethod() {
+    const button = document.getElementById('buttonRunContractMethod');
     button.disabled = true;
     try {
       _.checkExtensionAvailability();
       const provider = _.getProvider();
       const contract = new freeton.Contract(provider, Kington.abi, Kington.networks['2'].address);
-      const messages = await contract.functions.getMessages.runGet();
+      const messages = await contract.methods.getMessages.run();
       document.getElementById('result').innerHTML += '</br>' + JSON.stringify(messages);
       console.log(messages);
     } catch (e) {
@@ -82,7 +82,7 @@ window.app = {
       button.disabled = false;
     }
   },
-  async transfer() {//Transfer tokens to surf with message "Hello".
+  async transfer(form) {//Transfer tokens with message "Hello".
     const button = document.getElementById('buttonTransfer');
     button.disabled = true;
     try {
@@ -90,10 +90,8 @@ window.app = {
       const provider = _.getProvider();
       const signer = await provider.getSigner();
       const wallet = signer.getWallet();
-      const address = '0:b1c6ca063c050c4923580d3461c46c978651c4f100955e8ba32d40b278907206';
-      const amount = '100000000';
       const payload = 'te6ccgEBAgEADgABCAAAAAABAApIZWxsbw==';
-      const contractMessageProcessing = await wallet.transfer(address, amount, false, payload);
+      const contractMessageProcessing = await wallet.transfer(form.address.value, form.amount.value, false, payload);
       await contractMessageProcessing.wait();
       console.log(`Transferred. TxId: ${contractMessageProcessing.txid}`)
     } finally {
@@ -107,7 +105,6 @@ window.app = {
       _.checkExtensionAvailability();
       const provider = _.getProvider();
       const signer = await provider.getSigner();
-      console.log({publicKey: signer.getPublicKey()});
       const wallet = new freeton.Wallet(signer, form.address.value);
       const contractMessageProcessing = await wallet.confirmTransaction(form.txid.value);
       await contractMessageProcessing.wait();
@@ -116,14 +113,14 @@ window.app = {
       button.disabled = false;
     }
   },
-  async run() {
-    const button = document.getElementById('buttonRun');
+  async callContractMethod() {
+    const button = document.getElementById('buttonCallContractMethod');
     button.disabled = true;
     try {
       _.checkExtensionAvailability();
       const provider = _.getProvider();
       const signer = await provider.getSigner();
-      const contract = new freeton.Contract(signer, SetcodeMultisigWallet.abi, '0:138a41d0fafae5d6e9f7a19241702074e1f76858ebbe7bbc693bb8de813ca982');
+      const contract = new freeton.Contract(signer, SetcodeMultisigWallet.abi, '0:bc6ed51718ca3fe6ba96d84e8a0f2f409423b6e3f3198d08634cd7f74372ed48');
       const input = {
         dest: '0:11684118bc3062a07126191bf17a650dbb101aff809eb79a9c64b061f4b9b97b',
         value: '500000000',
@@ -131,10 +128,61 @@ window.app = {
         allBalance: false,
         payload: '',
       };
-      const result = await contract.functions.submitTransaction.run(input);
-      console.log(result);
+      const contractMessageProcessing = await contract.methods.submitTransaction.call(input);
+      await contractMessageProcessing.wait();
+      console.log(`Called. TxId: ${contractMessageProcessing.txid}`)
+      document.getElementById('result').innerHTML += '</br>' + `Called. TxId: ${contractMessageProcessing.txid}`
+    } catch (e) {
+      document.getElementById('result').innerHTML += '</br>' + JSON.stringify(e);
+      console.log(e);
     } finally {
       button.disabled = false;
     }
-  }
+  },
+  async getTokenList() {
+    const button = document.getElementById('buttonGetTokenList');
+    button.disabled = true;
+    try {
+      _.checkExtensionAvailability();
+      const provider = _.getProvider();
+      const signer = await provider.getSigner();
+      const wallet = signer.getWallet();
+      const tokenList = await wallet.getTokenList();
+      console.log(tokenList);
+    } catch (e) {
+      document.getElementById('result').innerHTML += '</br>' + JSON.stringify(e);
+      console.log(e);
+    } finally {
+      button.disabled = false;
+    }
+  },
+  async transferToken(form) {
+    const button = document.getElementById('buttonTransferToken');
+    button.disabled = true;
+    try {
+      _.checkExtensionAvailability();
+      const provider = _.getProvider();
+      const signer = await provider.getSigner();
+      const wallet = signer.getWallet();
+      const tokenList = await wallet.getTokenList();
+      let token = null;
+      for (const item of tokenList) {
+        if (item.rootAddress === form.rootAddress.value) {
+          token = item;
+        }
+      }
+      if (null === token) {
+        throw 'Token with this rootAddress not found in extension.';
+      }
+      const contractMessageProcessing = await token.transfer(form.address.value, form.amount.value);
+      await contractMessageProcessing.wait();
+      console.log(`Transferred. TxId: ${contractMessageProcessing.txid}`)
+      document.getElementById('result').innerHTML += '</br>' + `Transferred. TxId: ${contractMessageProcessing.txid}`
+    } catch (e) {
+      document.getElementById('result').innerHTML += '</br>' + JSON.stringify(e);
+      console.log(e);
+    } finally {
+      button.disabled = false;
+    }
+  },
 };
